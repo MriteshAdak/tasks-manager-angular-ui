7e658752251b1851a70c00f279931d49995819ad
MriteshAdak <mriteshadak@outlook.com>
2026-03-17 23:28:42 +0100

    Generated config files

---
+++ src/app/core/config/app-config-token.spec.ts
@@ -0,0 +1,7 @@
+import { AppConfigToken } from './app-config-token';
+
+describe('AppConfigToken', () => {
+  it('should create an instance', () => {
+    expect(new AppConfigToken()).toBeTruthy();
+  });
+});
