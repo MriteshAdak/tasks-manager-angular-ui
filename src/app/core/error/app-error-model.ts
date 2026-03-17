f3d4c3624d53e13734d0dbfd5da8bb970138f22d
MriteshAdak <mriteshadak@outlook.com>
2026-03-18 00:04:30 +0100

    Define AppErrorModel interface with properties for error handling

--- src/app/core/error/app-error-model.ts
+++ src/app/core/error/app-error-model.ts
@@ -1,1 +1,7 @@
-export interface AppErrorModel {}
+export interface AppErrorModel {
+    code: string;
+    message: string;
+    httpStatus: number;
+    requestId: string;
+    details?: Record<string, unknown>
+}
