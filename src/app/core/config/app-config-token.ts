739c636f580725e1a40d95da256928fc0fe1139b
MriteshAdak <mriteshadak@outlook.com>
2026-03-17 23:57:22 +0100

    Define AppConfigToken interface and create APP_CONFIG_TOKEN injection token

--- src/app/core/config/app-config-token.ts
+++ src/app/core/config/app-config-token.ts
@@ -1,1 +1,11 @@
-export class AppConfigToken {}
+import { InjectionToken } from '@angular/core';
+
+export interface AppConfigToken {
+    apiBasePath: string;
+    dateModelDefault: 'api' | 'local';
+    featureFlags: {
+        enableRuntimeModeOverride: boolean;
+    };
+}
+
+export const APP_CONFIG_TOKEN = new InjectionToken<AppConfigToken>("APP_CONFIG_TOKEN");
