35e35cd487742580dded4c0f021f2a12ccee9e2a
MriteshAdak <mriteshadak@outlook.com>
2026-03-18 15:12:54 +0100

    Implement initial routing and create button component with input properties

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        // Look here
+        loadComponent: () => import('./features/tasks/pages/pages').then(m => m.Pages)
+    }
+];
--- src/app/shared/components/ui/button/button.ts
+++ src/app/shared/components/ui/button/button.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   imports: [],
-  template: ` <p>button works!</p> `,
+  template: ` 
+  <button 
+    [type]="type()"
+    [disabled]="disabled()"
+    [class]="className()"
+    (click)="clicked.emit()">
+    <ng-content></ng-content>
+  </button> `,
   styles: ``,
 })
-export class Button {}
+export class Button {
+  readonly type = input<'button' | 'submit' | 'reset'>('button');
+  readonly disabled = input(false);
+  readonly className = input('');
+  readonly clicked = output<void>();
+}
