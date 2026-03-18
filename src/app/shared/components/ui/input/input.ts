a07a64281c54ca9e77aa5e58e41ee616488776f8
MriteshAdak <mriteshadak@outlook.com>
2026-03-18 15:29:12 +0100

    Implemented input component to use reactive properties and handle enter key events

--- src/app/shared/components/ui/input/input.ts
+++ src/app/shared/components/ui/input/input.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   imports: [],
-  template: ` <p>input works!</p> `,
+  template: ` 
+  <input 
+  [value]="value()" 
+  [placeholder]="placeholder()" 
+  [disabled]="disabled()" 
+  (keydown.enter)="enterPressed.emit((
+    $any($event.target).value ?? ''
+    ).toString())"
+  /> `,
   styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Input {}
+export class Input {
+  readonly value = input<string>("");
+  readonly placeholder = input<string>("");
+  readonly disabled = input<boolean>(false);
+  // readonly changed = output<string>();
+  readonly enterPressed = output<string>();
+}
