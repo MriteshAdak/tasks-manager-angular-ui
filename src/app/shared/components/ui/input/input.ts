9d05afeb2b53231ba5915a36a789675ff5098022
MriteshAdak <mriteshadak@outlook.com>
2026-03-18 16:18:27 +0100

    Refactor input component to remove unused enter key event handling and improve input propagation

--- src/app/shared/components/ui/input/input.ts
+++ src/app/shared/components/ui/input/input.ts
@@ -4,21 +4,36 @@ import { Component, ChangeDetectionStrategy, input, output } from '@angular/core
   selector: 'app-input',
   imports: [],
   template: ` 
-  <input 
-  [value]="value()" 
-  [placeholder]="placeholder()" 
-  [disabled]="disabled()" 
-  (keydown.enter)="enterPressed.emit((
-    $any($event.target).value ?? ''
-    ).toString())" /> 
+    <input 
+      [value]="value()" 
+      [placeholder]="placeholder()" 
+      [disabled]="disabled()" 
+      (input)="propogateChange($event)" 
+    /> 
   `,
-  styles: ``,
+  styles: `
+    input {
+      width: 100%;
+      height: 46px;
+      border: 1px solid #e2e8f0;
+      border-radius: 10px;
+      background: #f8fafc;
+      padding: 0 12px;
+      color: #475569;
+      outline: none;
+    }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Input {
   readonly value = input<string>("");
   readonly placeholder = input<string>("");
   readonly disabled = input<boolean>(false);
-  // readonly changed = output<string>();
-  readonly enterPressed = output<string>();
+  readonly changed = output<string>();
+  // readonly enterPressed = output<string>();
+
+  propogateChange(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value ?? "";
+    this.changed.emit(inputValue);
+  }
 }
